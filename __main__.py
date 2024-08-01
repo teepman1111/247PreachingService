90a42c89fb39dc0aae563f649bbe028085265b63
MiniOozy <nateteep5@gmail.com>
2024-08-01 10:04:32 -0500

    8/1 Morning Push

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ import os
 import json
 import googleapiclient.discovery
 from nested_lookup import nested_lookup
-import firebase
+from google.cloud import firestore
 
 def main():
     # Disable OAuthlib's HTTPS verification when running locally.
@@ -12,14 +12,14 @@ def main():
     api_service_name = "youtube"
     api_version = "v3"
     API_KEY = "AIzaSyCcrIQvSDBV7_LIBwV1xC0aG1o1HgLBxOk"
-    firebase = firebase.FirebaseApplication('',None)
+    channelid = "UC5RpnQM8-mvj_lDbM-P8Ggg"
     #sets up authenitcation method for YT API
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey = API_KEY)
     #initial request for channel details, must know channel id for now - looking into making this automated/more easily configurable
     channelrequest = youtube.channels().list(
         part="snippet,contentDetails,statistics",
-        id=input("Enter your value: "),
+        id=channelid,
         maxResults=50
         )
     channelresponse = channelrequest.execute()
@@ -37,16 +37,31 @@ def main():
     #makes a request to yt for videoIds based on the playlistId and fills an array with the ids
     def getvideoids(youtube, strippeduploadid):
         video_ids = []
-        videoidsrequest = youtube.playlistItems().list(part="contentDetails", playlistId=strippeduploadid, maxResults=50)
+        videoidsrequest = youtube.playlistItems().list(part="snippet, contentDetails", playlistId=strippeduploadid, maxResults=50)
         videoidsresponse = videoidsrequest.execute()
-        video_ids = nested_lookup('videoId', videoidsresponse)
+        for item in videoidsresponse['items']:
+             video_ids.append(item['contentDetails']['videoId'])
         next_page_token = videoidsresponse.get('nextPageToken')
         while next_page_token is not None:
             videoidsrequest = youtube.playlistItems().list(part="contentDetails", playlistId=strippeduploadid, maxResults=50,pageToken=next_page_token)
             videoidsresponse = videoidsrequest.execute()
-            video_ids = nested_lookup('videoId', videoidsresponse)
+            for item in videoidsresponse['items']:
+                    video_ids.append(item['contentDetails']['videoId'])
             next_page_token = videoidsresponse.get('nextPageToken')
         return video_ids
-    finalvideoids = getvideoids(youtube, strippeduploadid)
+    videoids = getvideoids(youtube, strippeduploadid)
+    def listtodict(listObj):
+        IDDict = {}
+        for index, item in enumerate(listObj):
+                IDDict[index] = item
+        return IDDict
+    IDDict = listtodict(videoids)
+    IDString = json.loads(json.dumps(IDDict))
+
+    db = firestore.Client(project='preaching247-b6740')
+
+    # Add a new doc in collection 'cities' with ID 'LA'
+    db.collection("YTVids").document(channelid).set(IDString)
+    
 if __name__ == "__main__":
     main()
